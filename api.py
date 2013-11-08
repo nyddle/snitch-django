4860032da51d6279c968ef1ff051719d22b84666
Andrey Rusanov <arusanov@envionsoftware.com>
2013-11-08 23:27:59 +0200

    Auth + reg

---
+++ api.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, request
+from itsdangerous import Signer
+from pymongo import MongoClient
+from pymongo.database import DBRef
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.sqlr
+
+
+@app.route('/api/get', methods=['POST', 'GET'])
+def get():
+    pass
+
+
+@app.route('/api/post', methods=['POST'])
+def post():
+    # todo: validation
+    with client.start_request():
+        prj = db.prjs.find_one({'token': request.json['app']})
+        if prj and '_id' in prj:
+            # todo: filter by user?
+            db.events.find({'project.$id': ObjectId(prj['_id'])})
+    return jsonify({'result': True})
+
+
+@app.route('/api/reg', methods=['POST'])
+def create_user():
+    # todo: move to decorator
+    # todo: validate email
+    # todo: send a letter
+    if request.json is None or not 'email' in request.json or not 'password':
+        return jsonify({'result': False, 'reason': 'wrong request'})
+    with client.start_request():
+        if db.users.find({'email': request.json['email']}).count() > 0:
+            return jsonify({'result': False, 'reason': 'already exists'})
+
+        s = Signer('ololo')
+        token = s.sign(request.json['email'])
+        db.users.insert({'token': token, 'email': request.json['email'],
+                         'password': generate_password_hash(request.json['password'])})
+    return jsonify({'result': True, 'token': token})
+
+
+@app.route('/api/prj', methods=['POST'])
+def create_proj():
+    if request.json is None or not 'title' in request.json or not 'token' in request.json:
+        return jsonify({'result': False, 'reason': 'wrong request'})
+    with client.start_request():
+        if db.prjs.find({'title': request.json['title']}).count() > 0:
+            return jsonify({'result': False, 'reason': 'already exists'})
+
+        user = db.users.find_one({'token': request.json['token']})
+        if not user:
+            return jsonify({'result': False, 'reason': 'not found'})
+        db.prjs.insert({'title': request.json['title'], 'owner': DBRef(collection='users', id=user['_id'])})
+    return jsonify({'result': True})
+
+
+@app.route('/api/auth', methods=['POST'])
+def auth():
+    if request.json is None or not 'email' in request.json or not 'password' in request.json:
+        return jsonify({'result': False, 'reason': 'wrong request'})
+
+    with client.start_request():
+        user = db.users.find_one({'email': request.json['email']})
+        if not user:
+            return jsonify({'result': False, 'reason': 'Email not found'})
+
+        if not check_password_hash(user['password'], request.json['password']):
+            return jsonify({'result': False, 'reason': 'Wrong password'})
+
+    return jsonify({'token': user['token'], 'result': True})
+
+if __name__ == '__main__':
+    app.run(debug=True)
